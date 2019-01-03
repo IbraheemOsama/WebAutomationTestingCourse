f9a9f1b371f1353a2746f3c9042f66d3aec835b5
Ibraheem Osama <ibraheem.osama@gmail.com>
2019-01-03 22:05:40 +1100

    Failing test

--- Workspace/Tax/Tax.Tests.UnitTests/Controllers/TaxControllerTests.cs
+++ Workspace/Tax/Tax.Tests.UnitTests/Controllers/TaxControllerTests.cs
@@ -29,6 +29,7 @@ namespace Tax.Tests.UnitTests.Controllers
 
             var result = await taxController.AddTax(new TaxViewModel());
 
+            Assert.True(false);
             Assert.True(taxController.ModelState.IsValid);
             Assert.IsType<ViewResult>(result);
             Assert.IsNotType<LocalRedirectResult>(result);
